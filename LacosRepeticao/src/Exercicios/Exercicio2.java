5e9c81903741e4475c6c02836e59961eb91ff885
Agnes Ruescas <60972489+Agnes-Ruescas@users.noreply.github.com>
2020-08-10 17:45:52 -0300

    Update Exercicio2.java

--- LacosRepeticao/src/Exercicios/Exercicio2.java
+++ LacosRepeticao/src/Exercicios/Exercicio2.java
@@ -14,19 +14,19 @@ public class Exercicio2 {
 			System.out.println("Digite o terceiro valor: ");
 			num3 = ler.nextInt();
 			
-			if(num1>num2 && num2>num3) {
+			if(num1>=num2 && num2>=num3) {
 				System.out.println(num3 + "," + num2+"," + num1);
 			}
-			else if(num1>num3 && num3>num2) {
+			else if(num1>=num3 && num3>=num2) {
 				System.out.println(num2 + "," + num3 + "," + num1);
 			}
-			else if(num2>num3 && num3>num1){
+			else if(num2>=num3 && num3>=num1){
 				System.out.println(num1 + "," + num3 + "," + num2);
 			}
-			else if(num2>num1 && num1>num3) {
+			else if(num2>=num1 && num1>=num3) {
 				System.out.println(num3 + "," + num1 + "," + num2);
 			}
-			else if(num3>num1 && num1>num2) {
+			else if(num3>=num1 && num1>=num2) {
 				System.out.println(num1 + "," + num2 + "," + num3);
 			}
 			else {
