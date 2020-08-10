08ef9e4b7c4b40b3c6d6434dca9774009e79216e
Agnes Ruescas <60972489+Agnes-Ruescas@users.noreply.github.com>
2020-08-10 17:44:31 -0300

    Update Exercicio1.java

--- LacosRepeticao/src/Exercicios/Exercicio1.java
+++ LacosRepeticao/src/Exercicios/Exercicio1.java
@@ -14,14 +14,14 @@ public class Exercicio1 {
 		System.out.println("Digite o terceiro valor: ");
 		num3 = ler.nextInt();
 		
-		if(num1>num2 && num2<num3) {
-			System.out.printf("O maior valor é: %d", num1);
+		if(num1>=num2 && num2<=num3) {
+			System.out.printf("O maior valor Ã©: %d", num1);
 		}
-		else if(num2>num1 && num1<num3) {
-			System.out.printf("O maior valor é: %d", num2);
+		else if(num2>=num1 && num1<=num3) {
+			System.out.printf("O maior valor Ã©: %d", num2);
 		}
 		else {
-			System.out.printf("O maior valor é: %d", num3);
+			System.out.printf("O maior valor Ã©: %d", num3);
 		}
 	}
 
