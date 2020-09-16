fdf7be9a0b54f6f82a1b9fcb43d72fdbdd177f16
Agnes-Ruescas <agnes.ruescas@outlook.com>
2020-09-16 09:00:21 -0300

    commit inicial

---
+++ SpringBoot/redeSocial/redeSocial/src/main/java/com/minhaRedeSocial/redeSocial/model/UserLogin.java
@@ -0,0 +1,46 @@
+package com.minhaRedeSocial.redeSocial.model;
+
+public class UserLogin {
+
+	private String nome;
+	
+	private String email;
+	
+	private String senha;
+	
+	private String token;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String usuario) {
+		this.email = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
